71b867235de51a871b989d2fe99a4d38c33aa6cb
Hexxsitoo19 <117560325+Hexxsitoo19@users.noreply.github.com>
2022-11-23 01:10:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert
+    ('en este dia especial quiero que la pases bien , espero aver sido el primero bueno, sabes que yo estare aqui para apoyarte en todo y sabes que me tienes ami para lo que sea  🌹 ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('ERES UNA PERSONA MUY INCREIBLE ENSERIO GRACIAS ALA VIDA POR HABERME ENCONTRADO CONTIGO <3 ')
+});
+
+yesBtn.addEventListener('click',function () {
+    alert
+    ('y te preguntaras como supe tu cumpleaños , pues la verdad algo me decia que era hoy Chii 🥺')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    (' y ahora preparate por que te voy a cantar')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    (' 3 ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    (' 2 ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    (' 1 ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('EL SOL ALAO TUYO NO ES TAN BRILLANTE ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('YO SOY DAVINCI Y TU MI OBRA DE ARTE<3')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('TU ERES ARTEEEEEEE !!! <3')
+});
+
+
+yesBtn.addEventListener('click',function () {
+    alert
+    ('Quieres mas ?')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('Ya Un poco mas')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('Ya esta bien')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('YO QUISIERA SER EL HUL Y TE PROTEJO<3')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('SUPERMAN PARA EXHIBIRTE EN EL UNIVERSO')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('SI FUESE BATMAN NO HABRIA NOCHES DE TEMOR')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('LA BATICUEVA NUESTRO NIDITO DE AMOR')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('SI YO FUERA EL HOMBRE ARAÑA POR UN BESO')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('CIEN PIES DE ALTURA ESCALO SIN ESFUERZO A TU BALCON')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('Y SI ALGUIEN TE PREGUNTA POR TU HEROE FAVORITO ')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('DILE QUE SOY YOOOOO !!! <3')
+});
+
+yesBtn.addEventListener('click',function () {
+    alert
+    (' Si si lo se es muy poco verdad jajaja')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    (' Pero llamame despues de que veas todo y te canto jijiji💖')
+});
+
+yesBtn.addEventListener('click',function () {
+    alert
+    (' Pero bueno se que hoy sera un gran dia ya veraz<3')
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('y te preguntaras como hize esto aunque es sencillo no es tanto pero es lo que cuenta , pero que sepa mas de esto te hare mas <3')
+
+});
+yesBtn.addEventListener('click',function () {
+    alert
+    ('PRESIONE "ACEPTAR" PARA ACEPTAR EL REGALO')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
